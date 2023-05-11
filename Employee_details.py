6c4d0c266494f4969238ffe848398ff20135f723
cipher <fynhacker@gmail.com>
2023-05-11 11:20:42 +0530

    first commit

--- Employee_details.py
+++ Employee_details.py
@@ -1,6 +1,5 @@
 import mysql.connector
 from flask import Flask,request,render_template,flash,redirect
-from tkinter import messagebox
 
 
 app=Flask(__name__)
@@ -9,15 +8,20 @@ app=Flask(__name__)
 def entry():
     return render_template('choice.html')
 
-@app.route("/entry",methods=['POST'])
+@app.route("/entry",methods=['POST','GET'])
 def enter():
     return render_template('entry.html')
 
-@app.route("/details",methods=['POST'])
+@app.route("/details",methods=['POST','GET'])
 def details():
     return render_template('get.html')
 
-@app.route("/submit",methods=['POST'])
+@app.route("/delete",methods=['POST','GET'])
+def delete():
+    return render_template('remove.html')
+
+
+@app.route("/submit",methods=['POST','GET'])
 def submit():
     if request.method=='POST':
         connect=mysql.connector.connect(
@@ -28,19 +32,25 @@ def submit():
 
         mycursor=connect.cursor()
         id=request.form['eid']
-        name=request.form['ename']
-        gender=request.form['egender']
-        role=request.form['erole']
-        location=request.form['elocation']
-        salary=request.form['esalary']
-        sql_2="select * from employees where emp_id={0}".format(id)
+        first_name=request.form['firstname']
+        last_name=request.form['lastname']
+        dob=request.form['dob']
+        gender=request.form['gender']
+        mobile=request.form['mobile']
+        email=request.form['email']
+        role=request.form['role']
+        location=request.form['location']
+        experience=request.form['experience']
+        salary=request.form['salary']
+        sql_2="select * from employee_details where id={0}".format(id)
         mycursor.execute(sql_2)
         result=mycursor.fetchall()
         
         mycursor2=connect.cursor()
-        sql_1="insert  into employees(EMP_ID,EMP_NAME,emp_gender,EMP_ROLE,emp_location ,EMP_SALARY) values(%s,%s,%s,%s,%s,%s)"
-        val=(id,name,gender,role,location,salary)
-        if (len(result)):  
+        sql_1="insert  into employee_details(id ,first_name ,last_name ,dob ,gender ,mobile_number , email, erole ,location ,experience,salary) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        val=(id,first_name,last_name,dob,gender,mobile,email,role,location,experience,salary)
+        if (len(result)): 
+         
             return render_template("sucess.html",message="Employee already Exist!")
         else:
             mycursor2.execute(sql_1,val)
@@ -51,7 +61,7 @@ def submit():
     connect.commit()
     connect.close()  
        
-@app.route("/get",methods=['POST']) 
+@app.route("/get",methods=['POST','GET']) 
 def get_details():
     if request.method=='POST':
         id=request.form['eid']
@@ -62,16 +72,38 @@ def get_details():
         database='clarivate')
         
         mycursor=connect.cursor()
-        sql_2="select * from employees where emp_id={0}".format(id)
+        sql_2="select * from employee_details where id={0}".format(id)
         mycursor.execute(sql_2)
         result=mycursor.fetchall()  
         connect.commit()
         connect.close() 
     if (len(result)):  
-        return render_template("display_details.html",id=result[0][0],name=result[0][1],gender=result[0][2],role=result[0][3],location=result[0][4],salary=result[0][5])
+        return render_template("display_details.html",id=result[0][0],first_name=result[0][1],last_name=result[0][2],dob=result[0][3],gender=result[0][4],mobile=result[0][5],email=result[0][6],role=result[0][7],location=result[0][8],experience=result[0][9],salary=result[0][10])
     else:
-        return render_template("no records.html")
+        return render_template("no records.html",message="No Records found!")
     
+    
+@app.route("/remove",methods=['POST','GET']) 
+def delete_details():
+    if request.method=='POST':
+        id=request.form['eid']
+        connect=mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='root',
+        database='clarivate')
+        
+        mycursor=connect.cursor()
+        sql_2="delete  from employee_details where id={0}".format(id)
+        mycursor.execute(sql_2)
+        result=mycursor.fetchall()  
+        connect.commit()
+        connect.close() 
+    if (len(result)):
+        return render_template("exist.html",message="Deleted Successfully!") 
+    else:
+         return render_template("exist.html",message="Deleted Successfully")
+       
     
 if __name__=='__main__':
     app.debug=True
